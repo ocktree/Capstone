6cc40846829df887d590cb554c824244c0a399ae
atyson816 <46214037+atyson816@users.noreply.github.com>
2018-12-28 02:13:08 -0600

    Initial commit

---
+++ Debug/driverlib/MSP430FR5xx_6xx/eusci_b_spi.d
@@ -0,0 +1,43 @@
+# FIXED
+
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: ../driverlib/MSP430FR5xx_6xx/eusci_b_spi.c
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: ../driverlib/MSP430FR5xx_6xx/inc/hw_memmap.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: Z:/ti/ccsv8/ccs_base/msp430/include/msp430.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: Z:/ti/ccsv8/ccs_base/msp430/include/msp430fr6989.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: Z:/ti/ccsv8/ccs_base/msp430/include/in430.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/intrinsics.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/intrinsics_legacy_undefs.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: Z:/ti/ccsv8/ccs_base/msp430/include/msp430fr5xx_6xxgeneric.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/stdint.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/sys/stdint.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/sys/cdefs.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/sys/_types.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/machine/_types.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/machine/_stdint.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/sys/_stdint.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/stdbool.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: ../driverlib/MSP430FR5xx_6xx/eusci_b_spi.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/assert.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/_ti_config.h
+driverlib/MSP430FR5xx_6xx/eusci_b_spi.obj: Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/linkage.h
+
+../driverlib/MSP430FR5xx_6xx/eusci_b_spi.c: 
+../driverlib/MSP430FR5xx_6xx/inc/hw_memmap.h: 
+Z:/ti/ccsv8/ccs_base/msp430/include/msp430.h: 
+Z:/ti/ccsv8/ccs_base/msp430/include/msp430fr6989.h: 
+Z:/ti/ccsv8/ccs_base/msp430/include/in430.h: 
+Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/intrinsics.h: 
+Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/intrinsics_legacy_undefs.h: 
+Z:/ti/ccsv8/ccs_base/msp430/include/msp430fr5xx_6xxgeneric.h: 
+Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/stdint.h: 
+Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/sys/stdint.h: 
+Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/sys/cdefs.h: 
+Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/sys/_types.h: 
+Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/machine/_types.h: 
+Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/machine/_stdint.h: 
+Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/sys/_stdint.h: 
+Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/stdbool.h: 
+../driverlib/MSP430FR5xx_6xx/eusci_b_spi.h: 
+Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/assert.h: 
+Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/_ti_config.h: 
+Z:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.4.LTS/include/linkage.h: 
